7f2e01b86ee50c44931f25c75da2852be23abc26
lizhe <lizhe19851001@126.com>
2016-07-29 00:11:14 +0800

    m

--- src/main/java/com/ccicnmg/certify/Application.java
+++ src/main/java/com/ccicnmg/certify/Application.java
@@ -1,12 +1,23 @@
 package com.ccicnmg.certify;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        logger.info("Running CommandLineRunner's run.");
+        logger.info("Some initial work can be done here.");
+    }
 }
