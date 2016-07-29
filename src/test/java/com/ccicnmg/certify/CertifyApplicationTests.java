61c6e4416270dd6eb68c91134aa2bba812d5e483
lizhe <lizhe19851001@126.com>
2016-07-30 02:52:26 +0800

    m

--- src/test/java/com/ccicnmg/certify/CertifyApplicationTests.java
+++ src/test/java/com/ccicnmg/certify/CertifyApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")//set the active profile to test
+@ActiveProfiles("test")
 public class CertifyApplicationTests {
 
     @Test
